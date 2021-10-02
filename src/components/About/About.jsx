fca46c8af9c516b9a2271f1e5b9d29c125f523d0
Gerald Tiamzon <gerald.tiamzon@yahoo.com>
2021-10-02 15:49:42 -0700

    added about me text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,18 @@
-import '../About/About.css'
+import "../About/About.css";
 
 const About = () => {
-    return(
-        <div id="navbar">
-        <strong>ABOUT ME HERE</strong> 
+  return (
+    <div id="navbar">
+        <img src="https://i.imgur.com/0ssjEaq.jpg" alt="" />
+        <div>
+            Former employee in the retail sector now a Software Developer with
+            skills in JavaScript, Python and React. I thrive in environments where
+            I’m constantly learning and I work hard to improve myself wherever I am.
+            With multiple years of communicating with both co-workers and customers
+            I have a strong foundation in communication and collaboration
         </div>
-        );
+    </div>
+  );
 };
 
 export default About;
